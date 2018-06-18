40c78d4006d7117124da9a131068d09797635071
Sachin Pande <sazap10@gmail.com>
2018-06-18 18:43:06 +0100

    Add simple webserver to keep app running

--- index.js
+++ index.js
@@ -148,3 +148,12 @@ client.on('message', message => {
 });
 
 client.login(token);
+
+const http = require('http');
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+}
+
+var server = http.createServer(requestListener);
+server.listen(8080);
